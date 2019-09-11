50d27ed3df38a5f7e30c8e5ff64e293d07db747f
Erik Beckstrand <erik.beckstrand@gmail.com>
2019-09-11 15:53:44 -0600

    add questions and answers variable

---
+++ assets/javascript/app.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+
+    var questions = [{
+        question: "A modulator-demodulator is a hardware device better known as what?",
+        answer1: "A processor",
+        answer2: "A printer",
+        answer3: "A modem",
+        answer4: "A monitor",
+        correctAnswer: 3
+    },
+
+    {
+        question: "What does the online acronym SMH stand for?",
+        answer1: "Shaking my head",
+        answer2: "Shackle mama's ham",
+        answer3: "Silent murder human",
+        answer4: "Salami mega hurt",
+        correctAnswer: 1
+    },
+
+    {
+        question: "What was the first publicly traded U.S. company to reach a $1 trillion market cap?",
+        answer1: "Microsoft",
+        answer2: "Apple",
+        answer3: "Facebook",
+        answer4: "Google",
+        correctAnswer: 2
+    },
+
+    {
+        question: "On which popular website do users send tweets?",
+        answer1: "Reddit",
+        answer2: "Amazon",
+        answer3: "Ebay",
+        answer4: "Twitter",
+        correctAnswer: 4
+    },
+
+    {
+        question: "Mark Zuckerberg was one of the founders of which social networking site?",
+        answer1: "Facebook",
+        answer2: "Instagram",
+        answer3: "MySpace",
+        answer4: "LinkedIn",
+        correctAnswer: 1
+    },
+
+    {
+        question: "What was the first console video game that allowed the game to be saved?",
+        answer1: "Final Fantasy",
+        answer2: "Dragon Quest",
+        answer3: "The Legend of Zelda",
+        answer4: "Tecmo Super Bowl",
+        correctAnswer: 3
+    },
+
+    {
+        question: "Created in 2009, what was the first decentralized cryptocurrency?",
+        answer1: "Ethereum",
+        answer2: "Ripple",
+        answer3: "Litecoin",
+        answer4: "Bitcoin",
+        correctAnswer: 4
+    },
+
+    {
+        question: "What do you call the small image icons used to express emotions or ideas in digital communication?",
+        answer1: "Digiemos",
+        answer2: "Emoji",
+        answer3: "Faces",
+        answer4: "Ideacons",
+        correctAnswer: 2
+    }];
+
+
+});
